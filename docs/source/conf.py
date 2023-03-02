4bfab85be6a1a2b3f51296de7c60f42911a1f847
guigui <guillaume.briffoteaux@gmail.com>
2023-03-02 19:10:15 +0100

    documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../examples'))
 
 import sphinx_rtd_theme
 
-autodoc_mock_imports = ["mpi4py", "pyro", "torch", "gpytorch"]
+autodoc_mock_imports = ["mpi4py", "pyro", "torch", "gpytorch", "pygmo"]
 
 # -- Project information -----------------------------------------------------
 
