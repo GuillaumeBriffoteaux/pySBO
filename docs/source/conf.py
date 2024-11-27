f70d73cb6dc735bdc6b9b550bd8b29ce3aa6b4a3
guigui <guillaume.briffoteaux@gmail.com>
2024-11-27 22:03:48 +0100

    update doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,8 @@ sys.path.insert(0, os.path.abspath('../../examples'))
 
 import sphinx_rtd_theme
 
-# autodoc_mock_imports = ["sklearn", "scipy", "numpy", "mpi4py", "pyro", "torch", "gpytorch", "pygmo", "tensorflow"]
+autodoc_mock_imports = ['mpi4py']
+
 
 # -- Project information -----------------------------------------------------
 
@@ -38,7 +39,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc' ,'sphinx.ext.autosummary', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc' ,'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
